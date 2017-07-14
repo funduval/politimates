bb7348eb6be8cc4b7db66d4972612c56d89fbfc3
funduval <fundaduval@gmail.com>
2017-07-14 14:43:10 -0400

    add comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,23 @@
 var friends = require('../data/friends.js');
-//need to gather survey answers as a variable
 
 module.exports = function (app) {
 
 app.get("/api/friends", function(req,res){
 
   res.json(friends);
+
 })
 
 app.post("/api/friends", function(req,res){
 
-  //if statements handling totalDifference
-console.log("You are most compatible with: ");
+//================declare variables "global" (to the post route, at least)
 
 var differences = []; 
 var allDiff = [];
 var bestCandidate;
 var arrayOne = [];
+
+//some functional/mapping logic to tease out the arrayOne and arrayTwo arrays to compare
 
 arrayOne.push(friends[friends.length-1])
 
@@ -29,6 +30,8 @@ arrayOne = arrayOne[0];
 var arrayTwo = friends.map(function(d){
 return d.scores
 });
+
+//subtract all of the different items in arrayOne from all of the correspondiing indexes in arrayTwo (for every single object in arrayTwo) which is the bigger object. Get the absolute values of the difference.//
 
 arrayTwo.map(function(object){
   
@@ -45,21 +48,23 @@ arrayTwo.map(function(object){
     allDiff.push(totDiff);
     
 });
+
+//Determine the smallest value (differences) in the array of all absolute differences (allDiff). This is the score of your best match.//
   
 Array.min = function( allDiff ){
     return Math.min.apply( Math, allDiff );
 };    
 
+//find the indexOf that score. It is also the index of the person in the whole friends data object.//
+
   bestCandidate = Array.min(allDiff);
   bestIndex = allDiff.indexOf(bestCandidate)
   bestCandidate = friends[bestIndex];
+
+  //here we will have to post it onto the pop-up modal
  
-  console.log(bestCandidate)
-  
+  console.log(bestCandidate) 
 
 });
-
-
-
 
 }
